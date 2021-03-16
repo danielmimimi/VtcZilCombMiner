2e1f0a1bbd64fda00b47057095905121ab7d6aee
Daniel Klauser <daniel.klauser@outlook.com>
2021-03-16 22:01:22 +0100

    Init

---
+++ CombMiner/MainPrg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombMiner
+{
+    public class MainPrg
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Arguments { get; set; } = string.Empty;
+
+        public StreamReader Reader { get; set; }
+
+        public void Start()
+        {
+            Process startInfo = new Process();
+            startInfo.StartInfo.CreateNoWindow = false;
+            startInfo.StartInfo.UseShellExecute = false;
+            startInfo.StartInfo.FileName = Path;
+            startInfo.StartInfo.Arguments = Arguments;
+            startInfo.StartInfo.RedirectStandardOutput = true;
+            //startInfo.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+            //{
+            //    // Prepend line numbers to each line of the output.
+            //    if (!String.IsNullOrEmpty(e.Data))
+            //    {
+            //        Console.WriteLine(e.Data);
+            //    }
+            //});
+            startInfo.Start();
+            // Reader = startInfo.StandardOutput;
+            startInfo.BeginOutputReadLine();
+            startInfo.WaitForExit();
+        }
+    }
+}
---
+++ CombMiner/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CombMiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ZilMiner = new MainPrg();
+            ZilMiner.Path = @"C:\Users\Daniel_Klauser\Desktop\ZilMiner\zilminer.exe";
+            ZilMiner.Arguments = "-P zil://zil1aazxenlf0298vp7z24rvyp5kk4fyveakg353aq.daniel@eu1-zil.shardpool.io:5000/api --report-hr=1 --work-timeout=99998 --retry-delay=998 --farm-retries=99998";
+
+            var VtcMiner = new MainPrg();
+            VtcMiner.Path = @"C:/Users/Daniel_Klauser/Desktop/ZilMiner/Vertcoin/vertcoin-ocm.exe";
+
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var canCelTokeSrcVtc = new CancellationTokenSource();
+
+            var t1 = new Task(() => ZilMiner.Start(),
+                    cancellationTokenSource.Token,
+                    TaskCreationOptions.LongRunning);
+
+            var t2 = new Task(() => VtcMiner.Start(),
+                    canCelTokeSrcVtc.Token,
+                    TaskCreationOptions.LongRunning);
+            t1.Start();
+            t2.Start();
+            while (true)
+            {
+                var input = Console.ReadLine();
+            }
+        }
+    }
+}
